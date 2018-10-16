a7e5a5e16a948b4465222ee715a5b5cc2d08deaf
nishisuke <nishisuke13+git@gmail.com>
2018-10-16 14:16:46 +0900

    Add rest operation

---
+++ lib/jsonapi/rest_operation.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'active_support/concern'
+
+module Jsonapi
+  module RestOperation
+    extend ActiveSupport::Concern
+
+    class_methods do
+      def list
+        response = client.get 'quotes'
+        body = response.body
+        json = JSON.parse(body, { symbolize_names: true })
+        res = Response.new(json)
+        res.data.map { |e| self.new(e, { included: res.included }) }
+      end
+
+      def find
+      end
+
+      def create
+      end
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+  end
+end
