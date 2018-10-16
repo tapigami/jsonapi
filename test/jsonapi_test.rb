d76e0db981331b07e9336e6977822f951ca2d167
nishisuke <nishisuke13+git@gmail.com>
2018-10-16 14:53:34 +0900

    test

--- test/jsonapi_test.rb
+++ test/jsonapi_test.rb
@@ -1,11 +1,31 @@
 require "test_helper"
 
+class Article < Jsonapi::Resource
+  has_one :author
+  has_many :comments
+end
+
+class Author < Jsonapi::Resource
+end
+
+class Comment < Jsonapi::Resource
+  has_one :author
+end
+
 class JsonapiTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Jsonapi::VERSION
   end
 
   def test_it_does_something_useful
-    assert false
+    assert Article.list.last.title == 'JSON API paints my bikeshed!'
+  end
+
+  def test_rel
+    assert Article.list.last.comments.last.body == 'I like XML better'
+  end
+
+  def test_srel
+    assert Article.list.last.author.firstName == 'Dan'
   end
 end
